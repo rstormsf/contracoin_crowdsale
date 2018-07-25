eaff63c2dc8fdc3572d6b862c683e58afb047c21
Gregory McCubbin <gwmccubbin@gmail.com>
2018-07-25 11:42:46 -0500

    Adds ropsten config

--- truffle.js
+++ truffle.js
@@ -1,19 +1,7 @@
-require('babel-register')
-require('babel-polyfill')
-
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -23,6 +11,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gasPrice: 25000000000,
+      network_id: 3
     }
   },
   solc: {
